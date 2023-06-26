ef086a9327285e4214480f129cd1ff6637e4342a
BurakKurt <krtburak@outlook.com>
2023-06-26 22:08:35 +0300

    project created

--- README.md
+++
@@ -1,1 +0,0 @@
-# react-fetch-fake-api
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</p>
+    </div>
+  );
+}
